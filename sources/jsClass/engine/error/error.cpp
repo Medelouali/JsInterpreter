35372eced30b27c2452806ab7c4bedc1359a15fa
Medelouali <Mohammedelouali1999@gmail.com>
2021-09-21 15:28:10 +0000

    adding error syntax handler 2

--- sources/jsClass/engine/error/error.cpp
+++ sources/jsClass/engine/error/error.cpp
@@ -4,6 +4,9 @@
 #include <typeinfo>
 #include "./tools/strings/strings.h"
 
+std::string phrase(std::string ph, std::string errorType="Syntax error"){
+	return errorType + ": " + ph;
+};
 //symetry of ", ', `, (, ), [, ], {, }
 bool paren(const std::string& base, char ch){
 
@@ -12,10 +15,12 @@ bool paren(const std::string& base, char ch){
 std::string symetry(const std::string& cmd){
 	std::string err="";
 	std::vector<char> vect{'"', '\'', '`', '(', ')', '[', ']', '{', '}'};
-	for(int i=0; i<3; i++)if(countChar(cmd, vect[i])%2)return "You forgot to open or close " +toStr(vect[i]);
+	for(int i=0; i<3; i++)
+		if(countChar(cmd, vect[i])%2)
+			return phrase("You forgot to open or close " +toStr(vect[i]));
 	for(int i=3; i<vect.size(); i++)
 		if(paren(cmd, vect[i])){
-			err="You forgot to open or close " + toStr(vect[i]);
+			err=phrase("You forgot to open or close " + toStr(vect[i]));
 			break;
 		};
 	return err;
@@ -30,20 +35,22 @@ std::string syntax(std::string str){
 	for(long unsigned int i=0; i<newStr.length()-1; i++){
 		long int index=indexOfChar(charsSet, newStr[i]);
 		if(i>0 && index==-1){
-			err=toStr(newStr[i])+ " is not allowed in Enigma commands as input, please read the docs";
+			err=phrase(toStr(newStr[i])+ 
+				" is not allowed in Enigma commands as input, please read the docs");
 			break;
 		};
 		long int index_1=indexOfChar(newCharsSet, newStr[i]);
 		long int index_2=indexOfChar(newCharsSet, newStr[i+1])-1;
 		if(index_1!=-1 && index_2!=-1 && errorMatrix[index_1][index_2]=='0'){
 			if(i==0)
-				err=toStr(newStr[i+1]) +
-					" can't be used in the beginning of a command, please read the docs";
+				err=phrase(toStr(newStr[i+1]) +
+				" can't be used in the beginning of a command, please read the docs");
 			else if(i==newStr.length()-2)
-				err=toStr(newStr[i]) + " can't be used at the end of a command, please read the docs";
+				err=phrase(toStr(newStr[i]) + 
+					" can't be used at the end of a command, please read the docs");
 			else{
-				err="The following characters can't be used in a sequence: "+ toStr(newStr[i])+
-				" and " + toStr(newStr[i+1]);
+				err=phrase("The following characters can't be used in a sequence: "+ toStr(newStr[i])+
+				" and " + toStr(newStr[i+1]));
 			};
 			break;
 		};
--- sources/jsClass/engine/error/tools/strings/strings.h
+++ sources/jsClass/engine/error/tools/strings/strings.h
@@ -9,5 +9,6 @@ long int indexOfChar(const std::string& base ,const char ch);
 std::string toStr(char ch);
 std::string cutOff(long unsigned int n1, long unsigned int n2, const std::string& base);
 std::string format(std::string base, ...);
+std::string phrase(std::string ph, std::string errorType="Syntax error");
 bool isCharIn(const std::string& base, char ch);
 
