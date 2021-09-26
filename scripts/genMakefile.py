73d7bc50a7e3ab0cdc2c3d3fe29169ac3390140c
Medelouali <Mohammedelouali1999@gmail.com>
2021-09-26 03:25:39 +0000

    separating scripts from the base code

--- scripts/genMakefile.py
+++ scripts/genMakefile.py
@@ -11,8 +11,10 @@ def getSources():
     filesNum=0
     script="CC=g++\n\n"
     paths=[]
-    for dirpath, dirfiles, namefiles in os.walk(os.getcwd()):
+    path=os.path.join(os.path.join(os.getcwd(), ".."), "sources")
+    for dirpath, dirfiles, namefiles in os.walk(path):
         for file in namefiles:
+            
             if(re.search(r".*cpp$", file)):
                 paths.append(f'f{filesNum}=.{os.path.join(dirpath, file).split("sources")[1]}')
                 filesNum+=1
@@ -20,7 +22,8 @@ def getSources():
     script+="\n\nall: compile\ncompile:\n"
     script+='\t@${CC} -g -o ./main.exe ' + f'{fileNums(len(paths))}'
     script+="\t@./main.exe\n"
+    print("Makefile has been generated...")
     return script
 
-with open("Makefile", "w") as file:
+with open("../sources/Makefile", "w") as file:
     file.write(getSources())
